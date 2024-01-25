1f66f575ea22ca0b98762f954883993a99e33891
Julie Dosher <juliegdosher@lewisu.edu>
2024-01-25 16:31:23 -0600

    added summary and closing remark

--- MatchMaker_jd.go
+++ MatchMaker_jd.go
@@ -18,6 +18,15 @@ const prompt3 = "I like playing board games: "
 const prompt4 = "I would appreciate handmade crochet gifts: "
 const prompt5 = "I am a morning person: "
 
+const scoreThresholdMin = 5
+const scoreThreshold1 = 65
+const scoreThreshold2 = 85
+const scoreThresholdMax = 100
+
+// each degree away from desired answer will remove 5 points from the score
+// example: desired answer = 5, given answer = 1 -> points to remove = 20 (4*5)
+const pointsPerVariance = 5
+
 func main() {
 	printIntro()
 	var in1 = getInput(prompt1)
@@ -26,9 +35,7 @@ func main() {
 	var in4 = getInput(prompt4)
 	var in5 = getInput(prompt5)
 
-	var score = getScore(in1, in2, in3, in4, in5)
-	fmt.Println("score:", score)
-
+	getTotalScore(in1, in2, in3, in4, in5)
 }
 
 func printIntro() {
@@ -70,15 +77,47 @@ func getInput(prompt string) int {
 	return numInput
 }
 
-func getScore(in1 int, in2 int, in3 int, in4 int, in5 int) int {
-	var score int = 100 // max score
-	score -= (2 * abs(ans1-in1))
-	score -= (2 * abs(ans2-in2))
-	score -= (2 * abs(ans3-in3))
-	score -= (2 * abs(ans4-in4))
-	score -= (2 * abs(ans5-in5))
+// adjusted method of determining final score, max score = 100%, min score = 5%
+func getTotalScore(in1 int, in2 int, in3 int, in4 int, in5 int) {
+	var totalScore int = 100 // max score
+	score1 := (pointsPerVariance * abs(ans1-in1))
+	score2 := (pointsPerVariance * abs(ans2-in2))
+	score3 := (pointsPerVariance * abs(ans3-in3))
+	score4 := (pointsPerVariance * abs(ans4-in4))
+	score5 := (pointsPerVariance * abs(ans5-in5))
 
-	return score
+	scores := [5]int{score1, score2, score3, score4, score5}
+
+	fmt.Printf("%s\n", strings.Repeat("*", 55))
+	for i := 0; i < len(scores); i++ {
+		totalScore -= scores[i]
+		printPromptScore(i+1, scores[i])
+	}
+
+	fmt.Printf("\nTotal Score: %v%%\n", totalScore)
+	printScoreSummary(totalScore)
+}
+
+func printPromptScore(promptNum int, score int) {
+	fmt.Printf("Prompt %v Score: %v%%\n", promptNum, 100*(20-score)/20)
+}
+
+func printScoreSummary(total int) {
+	if total == scoreThresholdMin {
+		fmt.Println("Wow... we are total opposites.")
+	} else if total == scoreThresholdMax {
+		fmt.Println("Wow... are we the same person?")
+	} else if total < scoreThreshold1 {
+		fmt.Println("We don't seem to be very compatible.")
+	} else if total < scoreThreshold2 {
+		fmt.Println("We seem to have a bit in common... maybe we could be friends!")
+	} else {
+		fmt.Println("We have a lot in common! Are we best friends now?")
+	}
+	fmt.Println()
+	fmt.Println("Thanks for using Matchmaker!")
+	fmt.Printf("%s\n", strings.Repeat("*", 55))
+	fmt.Println()
 }
 
 func abs(x int) int {
